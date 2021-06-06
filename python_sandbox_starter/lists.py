8fe143a5272aabc22325225fdd64a3a85770db62
Holger Mueller <9jholger@gmail.com>
2021-06-06 15:09:02 -0500

    tried some pyhton

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,19 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create list
+numbers = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'grapes', 'pears']
+
+# Constructor
+numbers2 = list((1, 2, 3, 4, 5))
+
+# Get value
+print(fruits[1])
+
+# Get length
+print(len(fruits))
+
+# Append
+fruits.append('mangoes')
+
+print(fruits)
